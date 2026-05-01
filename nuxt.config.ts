7b1702b2623798bff30024d4bcd8f321dd0033e8
nlabrazi <na.labrazi@gmail.com>
2026-05-02 01:22:37 +0200

    feat: Add request logging and utilities to server plugins
    - Implemented a new plugin for Nuxt3 that logs HTTP requests.
    - Added utility functions for serializing errors.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
         {
-          hid: "description",
+          key: "description",
           name: "description",
           content: "Jeux rétro jouables dans le navigateur",
         },
